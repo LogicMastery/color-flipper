71c89305547cfa546ed9de94f4a2d6dcc2d62944
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-05-07 21:28:38 +0800

    made a color generator

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const body = document.getElementsByTagName("body")
+const body = document.querySelector("body")
 
-function setColor(name){
-    body.style.backgroundColor = name;
+function setColorGreen(){
+    body.addEventListener('click', function(event){
+        body.style.backgroundColor = "green"
+    })
+}
+
+function setColorRed(){
+    body.addEventListener('click', function(event){
+        body.style.backgroundColor = "red"
+    })
+}
+
+function setColorBlue(){
+    body.addEventListener('click', function(event){
+        body.style.backgroundColor = "blue"
+    })
+}
+
+function randomColor(){
+    const red = Math.round(Math.random() * 255)
+    const green = Math.round(Math.random() * 255)
+    const blue = Math.round(Math.random() * 255)
+
+    const color = `rgb(${red}, ${green}, ${blue})`
+    body.style.backgroundColor = color;
 }
